00f6269bec850477e60fdca01ec9c2d95c068434
Fun117 <fun117.kun+dev@gmail.com>
2024-11-08 08:40:40 +0900

    First commit

---
+++ src/i18n/request.ts
@@ -0,0 +1,19 @@
+import config from "../../richtpl.config";
+import { getRequestConfig } from "next-intl/server";
+
+const locales = config.i18n.locales;
+
+export default getRequestConfig(async ({ requestLocale }) => {
+  // This typically corresponds to the `[locale]` segment
+  let locale = await requestLocale;
+
+  // Ensure that a valid locale is used
+  if (!locale || !config.i18n.locales.includes(locale as any)) {
+    locale = config.i18n.defaultLocale;
+  }
+
+  return {
+    locale,
+    messages: (await import(`../../messages/${locale}.json`)).default,
+  };
+});
